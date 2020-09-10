2b2bb5121d8edfb1591cd05b3aa2dca2329a694a
Calvin Smith <4283930+calvingsmith@users.noreply.github.com>
2020-09-10 18:37:37 -0400

    - Fixed broken merge

--- reset-sat.sh
+++ reset-sat.sh
@@ -6,10 +6,6 @@ hammer hostgroup delete --name "Coreos dev"
 
 hammer os delete --title "RHCOS 8"
 
-<<<<<<< HEAD
 hammer medium delete --name "Red Hat CoreOS" 
 
 hammer template delete --name "Red Hat CoreOS PXELinux"
-=======
->>>>>>> 1d3cfb5b5d61f820b5920c93c3b8050f99c47a96
-
